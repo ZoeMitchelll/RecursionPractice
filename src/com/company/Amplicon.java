33d2d7962ddd25fed64689dafa77d6ffcb4401c5
deemull <deemullins22@gmail.com>
2022-01-27 18:37:37 -0600

    Added comments to ZM's code additions. Added command line arguments.

--- src/com/company/Amplicon.java
+++ src/com/company/Amplicon.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Random;
 
-public class Amplicon {
+public class Amplicon implements Comparable<Object> {
 
     private int chromosome;
     private int start;
@@ -59,5 +59,10 @@ public class Amplicon {
 
     public int getEnd() {
         return end;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,15 @@ public class Main {
 
 
     public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("The command line arguments are:");
+            for (String val : args) {
+                System.out.println(val);
+            }
+        } else {
+            System.out.println("No command line arguments found.");
+        }
+
         //generate random DM with specified number of amplicons (intervals)
         System.out.printf("How many amplicons would you like to simulate per DM?  ");
         int amplicon = scan.nextInt();
